ed261eb3205183b666a010ea6dd34bbe492db848
Riccardo <riccardovicente10@gmail.com>
2022-05-31 17:45:43 +0200

    Corrected capital Outlay

--- Capital Outlay/Capital_Outlay.py
+++ Capital Outlay/Capital_Outlay.py
@@ -1,39 +1,39 @@
 import pandas as pd
 import numpy as np
+#-------------------------------------------------------------------------------------------------------
+#Renewable energy infrustructure
+#------------------------------------------------------------------------------------------------------
 
 # Defining Renewable energy costs ($/MWh)
-# LCOE = np.array([PV Energy (residential), 189], [PV Energy (large scale farm), 37], [Wind Energy, 40])
-
-pv_energy_residential = 189
-pv_energy_large = 37
-wind_energy = 40
-
 LCOE = {
     'pv_energy_residential': 189,
-    'pv_energy_large': 37,
+    'pv_energy_largescale': 37,
     'wind_energy': 40,
 }
-
-renewable_type = 'wind_energy'
-
-#Ask user renewabe energy specs
-RE_capacity = int(input("Enter RE capacity")) # MW
-print("{} MW".format(RE_capacity))
-
-eff = 0.1724 # %
-E_annual = RE_capacity*4380*eff
-E_annual = float(E_annual)
-print(E_annual)
-Lifetime = 25 # years
-
-E_lifetime = E_annual*Lifetime
-print(E_lifetime)
+renewable_type = 'pv_energy_largescale'
 
 USD_Rand = 15
 
-RE_cost = E_lifetime*LCOE[renewable_type]
-print(RE_cost)
+#Ask user renewable energy specs
+RE_capacity = float(input("Enter RE capacity ")) # MW
+print("Capacity: {} MW".format(RE_capacity))
 
+#Determine cost of RE over lifetime
+eff = 0.1724 # %
+E_annual = RE_capacity*4380*eff
+E_annual = float(E_annual)
+print("Annual Energy generation: {} MWh.".format(E_annual))
+Lifetime = 25 # years
+
+E_lifetime = E_annual*Lifetime
+print("Lifetime Energy generation: {} MWh".format(E_lifetime))
+
+RE_cost = E_lifetime*LCOE[renewable_type]*USD_Rand
+print("Capital Cost of RE infrustructure: R {:.2f}".format(RE_cost))
+
+#------------------------------------------------------------------------------------------
+#Crypto mining infrustructre
+#------------------------------------------------------------------------------------------
 
 # Get mining equipment data
 df = pd.read_excel('../Data/crypto_mining_equipment.xlsx')
--- Solar Irradiance/Solar_Irradiance.py
+++ Solar Irradiance/Solar_Irradiance.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
 
-print("Look, Mom, I'm writing in Python")
-
 df = pd.read_csv("../Data/Hourly_solar_irradiance_Bloemfontein.csv")
-df.head()
+print(df)
