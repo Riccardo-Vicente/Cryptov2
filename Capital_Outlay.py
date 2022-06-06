d228007c5cac72680d34a973a555036f457a5ea0
Riccardo <riccardovicente10@gmail.com>
2022-06-07 01:39:48 +0200

    Reading hardware excel and assigning cols to lists

--- Capital_Outlay.py
+++ Capital_Outlay.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from openpyxl.workbook import Workbook
+from  openpyxl import load_workbook
 #-------------------------------------------------------------------------------------------------------
 #Renewable energy infrustructure
 #------------------------------------------------------------------------------------------------------
@@ -30,28 +32,51 @@ print("Lifetime Energy generation: {} MWh".format(E_lifetime))
 
 RE_cost = E_lifetime*LCOE[renewable_type]*USD_Rand
 print("Capital Cost of RE infrustructure: R {:.2f}".format(RE_cost))
-
+print("\n")
 #------------------------------------------------------------------------------------------
 #Crypto mining infrustructre
 #------------------------------------------------------------------------------------------
 
 # Get mining equipment data
-df = pd.read_excel('./Data/crypto_mining_equipment.xlsx')
+#df = pd.read_excel('./Data/crypto_mining_equipment.xlsx')
+wb = load_workbook('./Data/crypto_mining_equipment.xlsx')
+ws = wb.active
 
-#Set hardware as index
-df.set_index(df["Hardware"].values)
-print(df)
+# Declare each excel column to a list to refer to later.
+miner_col = ws['C2':'C9']
+print("Mining Hardware:")
+miners = []
+for cell in miner_col:
+    for a in cell:
+        miners.append(a.value)
+print(miners)
+
+power_col = ws['E2':'E9']
+print("Power values:")
+power = []
+for cell in power_col:
+    for a in cell:
+        power.append(a.value)
+print(power)
+
+price_col = ws['D2':'D9']
+print("Miner Prices:")
+price = []
+for cell in price_col:
+    for a in cell:
+        price.append(a.value)
+print(price)
 
 #Declare miners to purchase
 miner1 = 'Canaan (Avalon 6)'
 miner2 = 'GeForce RTX 3060 Ti'
+print("\nSelected mining hardware:")
 print("Miner 1: {}".format(miner1))
 print("Miner 2: {}".format(miner2))
 
 #Get miner specs
-for ind, row in df:
-    power1 = row["Power Consumption (W)"]
-print(power1)
+
+
 
 
 
