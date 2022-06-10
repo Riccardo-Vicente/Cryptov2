1ca778ff84fec546022f0c8e88653f547d058f84
Riccardo <riccardovicente10@gmail.com>
2022-06-10 13:50:30 +0200

    Declared TOU, attempted calcs

--- Solar_Irradiance.py
+++ Solar_Irradiance.py
@@ -45,8 +45,44 @@ et = pd.read_csv(tariff_file)
 print(et)
 
 #Declare TOU tariffs
-
+Weekday_Tariff= {
+    1: 'OffPeak',
+    2: 'OffPeak',
+    3: 'OffPeak',
+    4: 'OffPeak',
+    5: 'OffPeak',
+    6: 'Standard',
+    7: 'Peak',
+    8: 'Peak',
+    9: 'Peak',
+    10: 'Standard',
+    11: 'Standard',
+    12: 'Standard',
+    13: 'Standard',
+    14: 'Standard',
+    15: 'Standard',
+    16: 'Standard',
+    17: 'Standard',
+    18: 'Peak',
+    19: 'Peak',
+    20: 'Standard',
+    21: 'Standard',
+    22: 'Off-Peak',
+    23: 'Off-Peak',
+}
 season = "LD"
 for ind, row in df.iterrows():
     if 5 <= row["Month"] <= 7:
         season = "HD"
+
+for ind, row in df.iterrows():
+    if row["Day"] = weekday
+    IPP_rev = Weekday_Tariff[row["Hour"]]*row["Solar Energy (kWh/h)"]/100
+
+    if row["Day"] = Saturday
+    IPP_rev = Sat_Tariff[row["Hour"]] * row["Solar Energy (kWh/h)"] / 100
+
+for ind, row in df.iterrows():
+    df.loc[ind, "Hourly Revenue (R)"] = Weekday_Tariff[row["Hour"]]*row["Solar Energy (kWh/h)"]/100
+
+print(round(df,2))
